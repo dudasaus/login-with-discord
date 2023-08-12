3050a7bbad67155b804d5d5506e7dfed0e620268
Ausitn <dudasaus@umich.edu>
2023-08-12 07:46:26 -0600

    getting tokens

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,46 @@
+import "dotenv/config";
 import express from "express";
 import { env } from "node:process";
+import request from "request";
 
 const PORT = env["PORT"] || 3000;
 
 const app = express();
 
+app.use("/", express.static("./src/static"));
+
 app.get("/", (_req, res) => {
-  return res.send("got it");
+  return res.send("got it!");
+});
+
+app.get("/authorize", (req, res) => {
+  const { code } = req.query;
+
+  const form = {
+    client_id: env["DISCORD_CLIENT_ID"],
+    client_secret: env["DISCORD_CLIENT_SECRET"],
+    grant_type: "authorization_code",
+    code,
+    redirect_uri: "http://localhost:3000/authorize",
+  };
+
+  request.post(
+    "https://discord.com/api/oauth2/token",
+    {
+      form,
+    },
+    (err, response, body) => {
+      if (!err) {
+        res.redirect("/home.html");
+      } else {
+        res.status(403).send("Unable to login");
+      }
+    }
+  );
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Not found");
 });
 
 app.listen(PORT, () => {
